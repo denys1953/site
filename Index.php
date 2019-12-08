5571741eb29961238160c0501ae93d544f13a901
denys1953 <51372398+denys1953@users.noreply.github.com>
2019-12-08 16:10:04 +0200

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Пример веб страницы</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    
+    
+    <style type="text/css">
+      div{
+        background: #E8F4FFFF;
+        padding: 10px;
+        border-left: 4px solid #BED1E6FF;
+      }
+    </style>
+    <?php
+    <p><a href="https://www.youtube.com/embed/hHW1oY26kxQ" target="color">1</a> |
+    <a href="https://www.youtube.com/embed/u6Aj-oZkl6Y" target="color">2</a></p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/u6Aj-oZkl6Y" name="color" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <div>Многоблочна структура</div>
+    
+      
+    
+    <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+    
+  </body>
+</html>
